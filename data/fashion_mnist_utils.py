Fix contour normalization + training data visualization

data/fashion_mnist_utils.py
@@ -2,11 +2,11 @@ import os
 from pathlib import Path
 import pandas as pd
 import numpy as np
-import scipy.io
 from tqdm import tqdm
 import tensorflow as tf
 import pickle
 import logging
+import imageio
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +17,8 @@ class FashionMnistUtils:
         self.test_csv = Path(self.pwd).joinpath("fashion-mnist_test.csv")
         self.train_csv = Path(self.pwd).joinpath("fashion-mnist_train.csv")
         self.image_shape = (28, 28)
+        self.pad_width = 4
+        self.pad_image_shape = tuple([dim + 2 * self.pad_width for dim in self.image_shape])
 
     def __load_data(self, partition="train", generate_noise=False):
         if partition is "train":
@@ -32,35 +34,37 @@ class FashionMnistUtils:
         labels = np.empty(num_samples)
         for idx in np.arange(num_samples):
             data[idx] = np.array(data_frame.iloc[[idx]]).reshape(self.image_shape) / 255.0
+            # pad the images
+            data[idx] = np.pad(data[idx], self.pad_width, mode="constant", constant_values=0)
             labels[idx] = int(data_frame.index[idx])
 
         # Adding a Gaussian noise to the images
         if generate_noise:
             for idx in np.arange(num_samples):
-                data[idx] += np.random.normal(loc=0, scale=0.1, size=self.image_shape)
+                data[idx] += np.random.normal(loc=0, scale=0.1, size=self.pad_image_shape)
 
-        return data, labels
+        return np.stack(data), labels
 
-    def __extract_corf_contours(self, batch_data):
+    def __extract_corf_contours(self, data):
         # Python to MATLAB connectivity
         # takes about 12 seconds
         import matlab.engine
         eng = matlab.engine.start_matlab()
         eng.cd(str(Path(self.pwd).parent.joinpath("CORFPushPull")), nargout=0)
 
-        batch_binary_map = np.empty_like(batch_data).astype(int)
-        batch_corf_response = np.empty_like(batch_data).astype(float)
-        mat_filename = str(Path(self.pwd).joinpath("cache/input_image.mat"))
-        for idx, img in tqdm(enumerate(batch_data)):
-            scipy.io.savemat(mat_filename, {"img": img})
+        binary_maps = np.empty_like(data).astype(int)
+        corf_response = np.empty_like(data).astype(float)
+        for idx, img in tqdm(enumerate(data)):
 
-            bm, cr = eng.contour_detection_from_python(1.0, 4.0, 1.8, 0.007, nargout=2)
+            # Compute the CORF response
+            matlab_img = matlab.double(initializer=img.tolist(), size=img.shape, is_complex=False)
+            bm, cr = eng.contour_detection_from_python(matlab_img, 1.0, 4.0, 1.8, 0.007, nargout=2)
 
-            batch_binary_map[idx] = np.array(bm._data).reshape(self.image_shape,
-                                                               order='F')  # MATLAB stores in col major order
-            batch_corf_response[idx] = np.array(cr._data).reshape(self.image_shape, order='F')
+            # MATLAB stores data in col major order
+            binary_maps[idx] = np.array(bm._data).reshape(self.pad_image_shape, order='F')
+            corf_response[idx] = np.array(cr._data).reshape(self.pad_image_shape, order='F')
 
-        return batch_binary_map, batch_corf_response
+        return binary_maps, corf_response
 
     def load_train_val_data(self, val_split=0.2, generate_noise=False):
         """
@@ -80,14 +84,21 @@ class FashionMnistUtils:
             with open(str(train_data_cache_file), "rb") as f:
                 grayscale_images, labels, _, corf_images = pickle.load(f)
         else:
+            # pad and load the images
             grayscale_images, labels = self.__load_data(partition="train", generate_noise=generate_noise)
             binary_maps, corf_images = self.__extract_corf_contours(grayscale_images)
+
+            # crop the images
+            grayscale_images = grayscale_images[:, self.pad_width:-self.pad_width, self.pad_width:-self.pad_width]
+            binary_maps = binary_maps[:, self.pad_width:-self.pad_width, self.pad_width:-self.pad_width]
+            corf_images = corf_images[:, self.pad_width:-self.pad_width, self.pad_width:-self.pad_width]
+
             with open(str(train_data_cache_file), 'wb') as f:
                 pickle.dump([grayscale_images, labels, binary_maps, corf_images], f)
 
         # Reshaping the arrays to match the tensorflow inputs of 4D arrays
         labels = labels.astype(int)
-        grayscale_images = np.expand_dims(np.stack(grayscale_images), axis=-1)
+        grayscale_images = np.expand_dims(grayscale_images, axis=-1)
         corf_images = np.expand_dims(corf_images, axis=-1)
 
         # Split the train data into train + validation
@@ -132,14 +143,21 @@ class FashionMnistUtils:
             with open(str(test_data_cache_file), "rb") as f:
                 grayscale_images, labels, _, corf_images = pickle.load(f)
         else:
+            # pad and load the images
             grayscale_images, labels = self.__load_data(partition="test", generate_noise=generate_noise)
             binary_maps, corf_images = self.__extract_corf_contours(grayscale_images)
+
+            # crop the images
+            grayscale_images = grayscale_images[:, self.pad_width:-self.pad_width, self.pad_width:-self.pad_width]
+            binary_maps = binary_maps[:, self.pad_width:-self.pad_width, self.pad_width:-self.pad_width]
+            corf_images = corf_images[:, self.pad_width:-self.pad_width, self.pad_width:-self.pad_width]
+
             with open(str(test_data_cache_file), 'wb') as f:
                 pickle.dump([grayscale_images, labels, binary_maps, corf_images], f)
 
         # Reshaping the arrays to match the tensorflow inputs of 4D arrays
         labels = labels.astype(int)
-        grayscale_images = np.expand_dims(np.stack(grayscale_images), axis=-1)
+        grayscale_images = np.expand_dims(grayscale_images, axis=-1)
         corf_images = np.expand_dims(corf_images, axis=-1)
 
         # Prepare the tensorflow data sets
@@ -153,14 +171,45 @@ class FashionMnistUtils:
         return test_grayscale, test_corf
 
     def save_generated_images(self):
-        pass
+
+        logger.info("Begin saving the clean training images to disk")
+        data_file = Path(self.pwd).joinpath("cache/f_mnist_clean_train.pkl")
+        if data_file.exists():
+            with open(str(data_file), "rb") as f:
+                grayscale_images, _, _, corf_maps = pickle.load(f)
+
+            for idx in np.arange(len(grayscale_images)):
+                # Save the clean images
+                img_file = Path(self.pwd).joinpath("cache/clean_images/{}.png".format(str(idx).zfill(5)))
+                imageio.imwrite(img_file, grayscale_images[idx])
+
+                # Save the corf maps generated from clean images
+                img_file = Path(self.pwd).joinpath("cache/corf_maps_from_clean_images/{}.png".format(str(idx).zfill(5)))
+                imageio.imwrite(img_file, corf_maps[idx])
+
+        logger.info("Begin saving the noisy training images to disk")
+        data_file = Path(self.pwd).joinpath("cache/f_mnist_noise_train.pkl")
+        if data_file.exists():
+            with open(str(data_file), "rb") as f:
+                grayscale_images, _, _, corf_maps = pickle.load(f)
+
+            for idx in np.arange(len(grayscale_images)):
+                # Save the noisy images
+                img_file = Path(self.pwd).joinpath("cache/noisy_images/{}.png".format(str(idx).zfill(5)))
+                imageio.imwrite(img_file, grayscale_images[idx])
+
+                # Save the corf maps generated from noisy images
+                img_file = Path(self.pwd).joinpath("cache/corf_maps_from_noisy_images/{}.png".format(str(idx).zfill(5)))
+                imageio.imwrite(img_file, corf_maps[idx])
 
 
 if __name__ == "__main__":
     data_utils = FashionMnistUtils()
-
     # Load the train data
     train_data = data_utils.load_train_val_data()
 
     # Load test data
     test_data = data_utils.load_test_data()
+
+    # Save the generated data for visualization
+    data_utils.save_generated_images()

models/cnn.py
@@ -14,13 +14,14 @@ class ConvolutionalNeuralNet1:
 
     def build_model(self):
         input_layer = tf.keras.Input(shape=(28, 28, 1), dtype=float, name="Input")
-        x = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation=tf.keras.activations.relu)(input_layer)
-        x = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation=tf.keras.activations.relu)(x)
-        x = tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(x)
+        x = tf.keras.layers.Conv2D(filters=20, kernel_size=(3, 3), activation=tf.keras.activations.relu,
+                                   kernel_regularizer=tf.keras.regularizers.l2(0.0001))(input_layer)
+        x = tf.keras.layers.Conv2D(filters=20, kernel_size=(3, 3), activation=tf.keras.activations.relu,
+                                   kernel_regularizer=tf.keras.regularizers.l2(0.0001))(x)
         x = tf.keras.layers.Flatten()(x)
 
-        x = tf.keras.layers.Dense(units=128, activation=tf.keras.activations.relu)(x)
-        x = tf.keras.layers.Dense(units=64, activation=tf.keras.activations.relu)(x)
+        x = tf.keras.layers.Dense(units=128, activation=tf.keras.activations.relu,
+                                  kernel_regularizer=tf.keras.regularizers.l2(0.01))(x)
         output_layer = tf.keras.layers.Dense(units=10, activation=tf.keras.activations.softmax)(x)
 
         self.model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
@@ -39,9 +40,9 @@ class ConvolutionalNeuralNet1:
             self.build_model()
             self.compile_model()
 
-        history = self.model.fit(x=train_data.batch(32), verbose=2,
+        history = self.model.fit(x=train_data.batch(128), verbose=2,
                                  callbacks=[SaveModelOnEpochEnd(file_prefix=file_prefix)],
-                                 epochs=30, validation_data=val_data.batch(32), initial_epoch=1,
+                                 epochs=150, validation_data=val_data.batch(128), initial_epoch=1,
                                  use_multiprocessing=True)
 
         # save the training history
@@ -92,7 +93,7 @@ class ConvolutionalNeuralNet1:
             title="Select the best {} model".format(file_prefix),
             filetypes=[("HDF5 Files", "*.h5")]
         )
-        # root.destroy()
+        logger.info("Selected best model: {}\n".format(selected_best_model))
 
         self.update_weights(selected_best_model)
 